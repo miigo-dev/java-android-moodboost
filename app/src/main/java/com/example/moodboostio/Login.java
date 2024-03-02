a24c5205901aaef54e6833294a332c89d8998199
miigo-dev <miigo.mnl@gmail.com>
2024-03-02 22:53:24 +0800

    implemented firebase auth (login)

--- app/src/main/java/com/example/moodboostio/Login.java
+++ app/src/main/java/com/example/moodboostio/Login.java
@@ -1,6 +1,12 @@
 package com.example.moodboostio;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,13 +14,69 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class Login extends AppCompatActivity {
+
+    Button btnLogin;
+    EditText editTextEmail, editTextPassword;
+    CheckBox rememberMe;
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        editTextEmail = findViewById(R.id.etEmail);
+        editTextPassword = findViewById(R.id.etPassword);
+        rememberMe = findViewById(R.id.cbRemember);
+
+        btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(view -> {
+            String email, password;
+            email = String.valueOf(editTextEmail.getText());
+            password = String.valueOf(editTextPassword.getText());
+
+            StringBuilder errorMessage = new StringBuilder();
+
+            if (TextUtils.isEmpty(email)) {
+                errorMessage.append("Enter Email\n");
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                errorMessage.append("Enter Password\n");
+            }
+
+            if (errorMessage.length() > 0) {
+                Toast.makeText(Login.this, errorMessage.toString(), Toast.LENGTH_SHORT).show();
+                return;
+            }
+
+            if (rememberMe.isChecked()) {
+                FirebaseUser currentUser = mAuth.getCurrentUser();
+                if (currentUser != null) {
+                    startActivity(new Intent(Login.this, MainActivity.class));
+                }
+            }
+
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(task -> {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(Login.this, "Successfully Logged-in.",
+                                    Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(Login.this, MainActivity.class));
+                        } else {
+                            Toast.makeText(Login.this, "Authentication failed.",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    });
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
